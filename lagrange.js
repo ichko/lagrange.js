5dd910ba8526f928c6db4c70bebde82f7fc304ce
Iliya Zhechev <i4ko95@gmail.com>
2016-10-13 00:19:30 +0300

    Refactoring and removing Point class

--- lagrange.js
+++ lagrange.js
@@ -10,30 +10,32 @@ class Point {
 class Lagrange {
 
     constructor(points = []) {
-        this.px = points.map((p) => p.x);
-        this.py = points.map((p) => p.y);
+        this.px = points.map((p) => p.x),
+        this.py = points.map((p) => p.y),
         this.polynomial = (x) => 1;
 
         this.buildPolynomial();
     }
 
     fullPolynomial(roots) {
-        return (x) => roots.reduce((product, xi) => product * (x - xi), 1);
+        return (x) => roots.reduce((product, xi) =>
+            product * (x - xi), 1);
     }
 
     splitPolynomial(i) {
-        let left = this.fullPolynomial(this.px.slice(0, i));
-        let right = this.fullPolynomial(this.px.slice(i + 1, this.px.length));
+        let left = this.fullPolynomial(this.px.slice(0, i))
+            right = this.fullPolynomial(this.px.slice(i + 1, this.px.length));
         return (x) => left(x) * right(x);
     }
 
     buildPolynomial() {
         let divisor = this.px.map((x, i) =>
-            this.splitPolynomial(i)(x), this);
+            this.splitPolynomial(i)(x), this),
+            me = this;
 
         return this.polynomial = (x) => 
             this.py.reduce((sum, y, i) =>
-                sum + y * this.splitPolynomial(i)(x) / divisor[i]);
+                sum + y * me.splitPolynomial(i)(x) / divisor[i]);
     }
 
 }
