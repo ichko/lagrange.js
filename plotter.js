58747e4818dd7b4c7e6119d8771b13199881ebc8
Iliya Zhechev <i4ko95@gmail.com>
2016-10-13 18:07:12 +0300

    Refactoring plotter and implementing the example

--- plotter.js
+++ plotter.js
@@ -1,39 +1,50 @@
 class Plotter {
 
-    constructor(ctx, config = { 
-        from: 0, to: 256,
-        color: 'black', lineSize = 2,
-        stepSize = 10,
-        scale = 1,
-        origin = { x: 0, y: 0 }
+    constructor(ctx, config = {
+        from: -800, to: 800,
+        color: 'blue',
+        lineSize: 2,
+        stepSize: 10,
+        scale: 10,
+        origin: { x: 0, y: 0 }
     }) {
         this.ctx = ctx;
+        this.config = config;
+        this.functions = [];
     }
 
-    plot(functions) {
-        functions.forEach((func) => {
+    addFunction(func) {
+        this.functions.push(func);
+        return this;
+    }
+
+    show() {
+        this.functions.forEach((func) => {
             this.plotFunction(func);
         }, this);
     }
 
     plotFunction(func) {
-        let formY = func(this.config.from / this.config.scale + this.config.origin.x) * this.config.scale + this.config.origin.y;
+        let fromY = func(this.config.from / this.config.scale + this.config.origin.x) * this.config.scale + this.config.origin.y;
         for (let x = this.config.from + this.config.stepSize;x < this.config.to;x += this.config.stepSize) {
             let toX = x + this.config.origin.x;
             let fromX = toX - this.config.stepSize;
-            let toY = func(x / this.config.scale + this.config.origin.x) * this.scale  + this.config.origin.y;
+            let toY = func(x / this.config.scale + this.config.origin.x) * this.config.scale  + this.config.origin.y;
 
             this.drawLine(fromX, fromY, toX, toY);
-            formY = toY;
+            fromY = toY;
         }
-
     }
 
     drawLine(fromX, fromY, toX, toY) {
+        this.ctx.strokeStyle = this.config.color;
+        this.ctx.lineWidth = this.config.lineSize;
+
         this.ctx.beginPath();
-        this.ctx.moveTo(fromX, fromY);
-        this.ctx.lineTo(toX, toY);
+        this.ctx.moveTo(fromX, -fromY);
+        this.ctx.lineTo(toX, -toY);
         this.ctx.stroke();
+        this.ctx.closePath();
     }
 
 }
