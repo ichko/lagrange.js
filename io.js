ddb7d3733b0985daf4706a94067315f3d79fddae
Iliya Zhechev <i4ko95@gmail.com>
2016-10-14 22:52:10 +0300

    IO implementing key bindings

--- io.js
+++ io.js
@@ -7,12 +7,29 @@ class IO {
             scroll: () => {},
             click: () => {}
         };
-
+        this.keyBindings = {};
         this.registerBindings();
+    }
+
+    onKey(key, handler) {
+        this.keyBindings[key] = { isDown: false, handler };
+
+        return this;
+    }
+
+    invokeHandlers() {
+        for (let key in this.keyBindings){
+            if(this.keyBindings[key].isDown){
+                this.keyBindings[key].handler();
+            }
+        }
+
+        return this;
     }
 
     registerBindings() {
         let me = this
+
         window.addEventListener('mousewheel', (event) => {
             me.mouse.scroll(Math.sign(event.wheelDelta));
         });
@@ -20,7 +37,18 @@ class IO {
         window.addEventListener('click', (event) => {
             me.mouse.click({ x: event.x, y: event.y });
         });
-    }
 
+        window.addEventListener('keydown', (event) => {
+            if(me.keyBindings[event.key]) {
+                me.keyBindings[event.key].isDown = true;
+            }
+        });
+
+        window.addEventListener('keyup', (event) => {
+            if(me.keyBindings[event.key]) {
+                me.keyBindings[event.key].isDown = false;
+            }
+        });
+    }
 
 }
