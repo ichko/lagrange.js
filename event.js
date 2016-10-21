548589c9683d463627c29701a6589059a2e74065
Iliya Zhechev <i4ko95@gmail.com>
2016-10-21 18:11:39 +0300

    Refactoring event.js

--- event.js
+++ event.js
@@ -2,32 +2,45 @@ class Event {
 
     constructor() {
         this.eventInstances = [];
-        this.create = (name, condition, contextProvider) => {
-            let binds = [];
-            this.eventInstances.push({ binds, condition, contextProvider, name });
-            this.attach[name] = {
-                to: (elements, handler, antiHandler) => binds.push({ elements, handler, antiHandler })
-            };
-            
-            return this;
-        }
-        
         this.attach = {}
     }
 
-    invokeHandlers() {
+    create(name, condition, contextProvider) {
+        let binds = [];
+        this.eventInstances.push({ binds, condition, contextProvider, name });
+        this.attach[name] = this.buildAttachobject(binds);
+        
+        return this;
+    }
+
+    buildAttachobject(binds) {
+        return {
+            to: (elements, handler, antiHandler) => {
+                binds.push({ elements, handler, antiHandler });
+                return this;
+            }
+        };
+    }
+
+    trigger() {
         this.self = this;
         this.eventInstances.forEach(({ binds, condition, contextProvider }) => {
             var context = contextProvider && contextProvider();
-            binds.forEach(({ elements, handler, antiHandler }) => {
-                elements.forEach((element) => {
-                    if(condition(element, context)) {
-                        handler(element, context);
-                    } else{
-                        antiHandler && antiHandler(element, context);
-                    }
-                });
-            });
+            binds.forEach(({ elements, handler, antiHandler }) =>
+                this.invokeBindsHandlers(
+                    elements, condition,
+                    handler, antiHandler, context
+                ));
+        });
+    }
+
+    invokeBindsHandlers(elements, condition, handler, antiHandler, context) {
+        elements.forEach((element) => {
+            if(condition(element, context)) {
+                handler(element, context);
+            } else {
+                antiHandler && antiHandler(element, context);
+            }
         });
     }
 
